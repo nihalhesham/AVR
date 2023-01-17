7b99fac98249d85a7f594680775916ce02d840df
Nihal <nihalheshammohamed@gmail.com>
2023-01-18 01:20:56 +0200

    UART

---
+++ MCAL/UART/USART_Config.h
@@ -0,0 +1,18 @@
+/*
+ * USART_Config.h
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: lenovo
+ */
+
+#ifndef USART_CONFIG_H_
+#define USART_CONFIG_H_
+/*
+ * PARITY_DISABLE
+ * PARITY_ODD
+ * PARITY_EVENs
+ * */
+#define PARITY_MODE PARITY_DISABLE
+
+
+#endif /* USART_CONFIG_H_ */
---
+++ MCAL/UART/USART_Interface.h
@@ -0,0 +1,18 @@
+/*
+ * USART_Interface.h
+ *
+ *  Created on: ??þ/??þ/????
+ *      Author: lenovo
+ */
+
+#ifndef USART_INTERFACE_H_
+#define USART_INTERFACE_H_
+
+void USART_Init( void );
+void USART_Transmit( u8 data );
+u8 USART_Receive( void );
+
+
+
+
+#endif /* USART_INTERFACE_H_ */
